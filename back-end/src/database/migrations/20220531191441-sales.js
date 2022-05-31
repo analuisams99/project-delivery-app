e5e93e967971855f5ec50b5ca603d1e40eb2d6d3
Luiz Fernando C. Modolo <87335906+LuizModolo@users.noreply.github.com>
2022-05-31 16:29:16 -0300

    criação: criando as migrations do sequelize

---
+++ back-end/src/database/migrations/20220531191441-sales.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+      },
+      sellerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'seller_id',
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL,
+        field: 'total_price',
+        allowNull: false,
+      },
+      deliveryAdress: {
+        type: Sequelize.STRING,
+        field: 'delivery_adress',
+        allowNull: false,
+      },
+      deliveryNumber: {
+        type: Sequelize.STRING,
+        field: 'delivery_number',
+        allowNull: false,
+      },
+      saleDate: {
+        type: Sequelize.DATE,
+        field: 'sale_date',
+        allowNull: false,
+        default: Sequelize.NOW,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('sales');
+  }
+};
---
+++ back-end/src/database/migrations/20220531192422-salesProducts.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    queryInterface.createTable('salesProducts', {
+      saleId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sales',
+          key: 'id',
+        },
+        field: 'sale_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'products',
+          key: 'id',
+        },
+        field: 'product_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('salesProducts');
+  }
+};
