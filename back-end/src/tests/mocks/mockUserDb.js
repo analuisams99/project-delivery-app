f64b93766020f9766f544ac55b739be389d41834
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-06-03 11:26:01 -0300

    atualizacao: Algumas correções de erros

--- back-end/src/tests/mocks/mockUserDb.js
+++ back-end/src/tests/mocks/mockUserDb.js
@@ -1,20 +1,33 @@
-module.exports = [
-  {
-      id: 1,
-      name: "Delivery App Admin",
-      email: "adm@deliveryapp.com",
-      role: "administrator"
-  },
-  {
-      id: 2,
-      name: "Fulana Pereira",
-      email: "fulana@deliveryapp.com",
-      role: "seller"
-  },
-  {
-      id: 3,
-      name: "Cliente Zé Birita",
-      email: "zebirita@email.com",
-      role: "customer"
-  },
+const usersDb= [
+    {
+        id: 1,
+        name: "Delivery App Admin",
+        email: "adm@deliveryapp.com",
+        role: "administrator"
+    },
+    {
+        id: 2,
+        name: "Fulana Pereira",
+        email: "fulana@deliveryapp.com",
+        role: "seller"
+    },
+    {
+        id: 3,
+        name: "Cliente Zé Birita",
+        email: "zebirita@email.com",
+        role: "customer"
+    },
+];
+const sellersDb= [
+    {
+        id: 2,
+        name: "Fulana Pereira",
+        email: "fulana@deliveryapp.com",
+        role: "seller"
+    },
 ]
+
+module.exports = { 
+    usersDb,
+    sellersDb,
+}
