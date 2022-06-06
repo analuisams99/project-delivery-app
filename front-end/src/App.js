2cf08d2ac2d7e7da1824a1f2d2929cd4a5a31b42
Luiz Fernando C. Modolo <87335906+LuizModolo@users.noreply.github.com>
2022-06-06 15:02:59 -0300

    Atualização: ajuste no direcionamento de rotas do login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,21 +10,24 @@ import {
   SellerOrders,
   SellerOrderDetails,
   AdminManage,
+  NotFound,
 } from './pages';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={ <Navigate to="/login" /> } />
-      <Route path="/login" element={ <Login /> } />
-      <Route path="/register" element={ <Register /> } />
-      <Route path="/customer/products" element={ <Products /> } />
-      <Route path="/customer/checkout" element={ <Checkout /> } />
-      <Route path="/customer/orders" element={ <CustomerOrders /> } />
-      <Route path="/customer/orders/:id" element={ <CustomerOrderDetails /> } />
-      <Route path="/seller/orders" element={ <SellerOrders /> } />
-      <Route path="/seller/orders/:id" element={ <SellerOrderDetails /> } />
-      <Route path="/admin/manage" element={ <AdminManage /> } />
+      <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/register" element={ <Register /> } />
+      <Route exact path="/customer/products" element={ <Products /> } />
+      <Route exact path="/customer/checkout" element={ <Checkout /> } />
+      <Route exact path="/customer/orders" element={ <CustomerOrders /> } />
+      <Route exact path="/customer/orders/:id" element={ <CustomerOrderDetails /> } />
+      <Route exact path="/seller/orders" element={ <SellerOrders /> } />
+      <Route exact path="/seller/orders/:id" element={ <SellerOrderDetails /> } />
+      <Route exact path="/admin/manage" element={ <AdminManage /> } />
+      <Route exact path="/admin/manage" element={ <AdminManage /> } />
+      <Route path="/*" element={ <NotFound /> } />
     </Routes>
   );
 }
