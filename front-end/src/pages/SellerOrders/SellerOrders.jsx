89726f7e33ba4dbf8e45bf749278fb48d25c0472
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-06-07 18:15:43 -0300

    implementação: tela de orders de sellers

--- front-end/src/pages/SellerOrders/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders/SellerOrders.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
+import OrdersCard from '../../components/OrdersCard';
+import { getSellerOrders } from '../../services/api';
 
 function SellerOrders() {
-  const userJSON = localStorage.getItem('user');
-  const user = JSON.parse(userJSON);
+  const user = JSON.parse(localStorage.getItem('user'));
+  const userId = JSON.parse(localStorage.getItem('userId'));
+  const [orders, setOrders] = useState([]);
 
   const ordersButton = {
     name: 'Pedidos',
@@ -11,10 +14,25 @@ function SellerOrders() {
     role: 'seller/orders',
   };
 
+  const getAllOrders = async () => {
+    const allOrders = await getSellerOrders(user.token, userId.id);
+    setOrders(allOrders);
+  };
+
+  const SELLER = 'seller';
+
+  useEffect(() => {
+    getAllOrders();
+  }, []);
+
   return (
     <div>
       <Header buttons={ [ordersButton] } userName={ user.name } />
-      <h1>SellerOrders</h1>
+      {
+        !orders.status
+          ? <OrdersCard orders={ orders } role={ SELLER } />
+          : <p>Nenhum produto encontrado</p>
+      }
     </div>
   );
 }
