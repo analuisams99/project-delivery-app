fc977980fa19e7a340c293c49318882fa82c42bd
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-06-09 10:06:43 -0300

    Atualizacao - Nome da Tabela  sales_product

--- back-end/src/database/models/saleProduct.js
+++ back-end/src/database/models/saleProduct.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const SaleProduct = sequelize.define('SaleProduct',
   {
     quantity: DataTypes.INTEGER,
-  }, { timestamps: false, tableName: 'salesProducts', underscored: true });
+  }, { timestamps: false, tableName: 'sales_products', underscored: true });
 
   SaleProduct.associate = (models) => {
     models.Sale.belongsToMany(models.Product,
